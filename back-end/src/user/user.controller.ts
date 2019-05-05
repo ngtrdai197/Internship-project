e478cc844289052907777e6659b82c9f012f5cfc
Nguyen Dai <ngtrdai290197@gmail.com>
2019-05-05 23:59:09 +0700

    CRUD for users :heavy_check_mark

--- back-end/src/user/user.controller.ts
+++ back-end/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, UseInterceptors, UploadedFile, UploadedFiles, UseGuards, SetMetadata } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UseInterceptors, UploadedFile, UploadedFiles, UseGuards, SetMetadata, Delete, Put } from '@nestjs/common';
 import { FilesInterceptor, FileInterceptor } from '@nestjs/platform-express'
 import { CreateUserDto } from '../dto/user.dto';
 import { UsersService } from './user.service';
@@ -6,11 +6,18 @@ import { RolesGuard } from 'src/common/guards/roles.guard';
 import { AuthGuard } from '@nestjs/passport';
 import { diskStorage } from 'multer';
 import { ApiUseTags, ApiOperation, ApiResponse, ApiCreatedResponse } from '@nestjs/swagger';
+import { User } from 'src/interfaces/user.interface';
 
 @ApiUseTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
+
+  @Post('upload')
+  @UseInterceptors(FilesInterceptor('files'))
+  uploadFile(@UploadedFiles() files) {
+    console.log(files);
+  }
 
   @ApiOperation({ title: 'Create User' })
   @ApiResponse({
@@ -18,14 +25,13 @@ export class UsersController {
     description: 'The record has been successfully created.',
   })
   @Post('create')
-  async create(@Body() createUserDto: CreateUserDto) {
-    this.usersService.create(createUserDto);
+  async create(@Body() createUserDto: CreateUserDto): Promise<Object> {
+    return this.usersService.create(createUserDto);
   }
 
-  @Post('upload')
-  @UseInterceptors(FilesInterceptor('files'))
-  uploadFile(@UploadedFiles() files) {
-    console.log(files);
+  @Put('update/:id')
+  async updateUser(@Param('id') id: string, @Body() updateUserDto: CreateUserDto):Promise<Object>{
+    return await this.usersService.update(id, updateUserDto);;
   }
 
   @UseGuards(AuthGuard('jwt'), RolesGuard)
@@ -34,8 +40,8 @@ export class UsersController {
   @ApiResponse({ status: 403, description: 'You do not have permission (Roles).' })
   @ApiCreatedResponse({ type: CreateUserDto })
   @Get('api')
-  async findAll() {
-    return this.usersService.findAll();
+  async findAll(): Promise<User[]> {
+    return await this.usersService.findAll();
   }
 
   @Get('api/:id')
@@ -44,4 +50,9 @@ export class UsersController {
     return this.usersService.findOneById(id);
   }
 
+  @Delete('api/:id')
+  // @ApiCreatedResponse({ type: CreateUserDto })
+  async findUserAndRemove(@Param('id') id: string): Promise<object> {
+    return this.usersService.delete(id);
+  }
 }
